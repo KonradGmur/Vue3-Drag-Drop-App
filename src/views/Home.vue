b8663dd708f67761a1a7b22c087afdcaf245af4a
Konrad <konrad.gmur@gmail.com>
2021-08-25 20:00:59 +0200

    Adding styles to the project part 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,44 @@
 <template>
 	<div class="home">
+		<h1>DragDrop</h1>
 		<DragDrop />
+		<span class="file-info">File: {{ dragdropFile.name }}</span>
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import DragDrop from '@/components/DragDrop.vue';
+import { ref } from 'vue';
 
 export default {
 	name: 'Home',
 	components: {
 		DragDrop,
 	},
+	setup() {
+		let dragdropFile = ref('');
+
+		return { dragdropFile };
+	},
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #f1f1f1;
+}
+
+h1 {
+	font-size: 40px;
+	margin-bottom: 32px;
+}
+
+.file-info {
+	margin-top: 32px;
+}
+</style>
