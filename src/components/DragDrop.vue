cd08d44ed0d7ae1e92766f6cf34a5b588e54944a
Konrad <konrad.gmur@gmail.com>
2021-08-25 19:30:08 +0200

    Project preparation and removal unnecessary file

---
+++ src/components/DragDrop.vue
@@ -0,0 +1,11 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: 'DragDrop',
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<DragDrop />
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DragDrop from '@/components/DragDrop.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'Home',
+	components: {
+		DragDrop,
+	},
+};
 </script>
